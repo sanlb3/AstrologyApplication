374483455a9555f17efa9ce475341366023eeaa6
sanlb3 <sanlb2810@gmail.com>
2022-12-04 18:57:47 -0800

    Initial commit

---
+++ app/src/main/java/com/example/astrologyapplication/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.astrologyapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2500)
+    }
+}
